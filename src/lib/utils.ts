8be236c1789faa15ebdade91cb96495f0fb0a17a
Simon <simon21.blum@gmail.com>
2025-01-20 08:48:46 +0100

    test: add sanity test checks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function sanity_check(a: number, b: number): number {
+	return a + b;
+}
