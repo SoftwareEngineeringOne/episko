3abdc375da4df2e58541d0c2f9ee8cc03f017a9e
Simon <simon21.blum@gmail.com>
2025-02-15 09:23:33 +0100

    refactor: update structs for config

---
+++ episko_lib/src/config/config_handler.rs
@@ -0,0 +1,16 @@
+use std::path::{Path, PathBuf};
+
+use super::{Config, Result};
+
+pub struct ConfigHandler;
+
+impl ConfigHandler {
+    pub fn load_config() -> Result<Config> {
+        todo!()
+    }
+
+    pub fn save_config() -> Result<()> {
+        todo!()
+    }
+}
+
