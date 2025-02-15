eed8914db4520b40f6f7e2a3fb9814fdca3780fe
Simon <simon21.blum@gmail.com>
2025-02-15 10:59:17 +0100

    feat: update config and config_handler

--- episko_lib/src/config.rs
+++ episko_lib/src/config.rs
@@ -1,29 +1,91 @@
-use std::path::{Path, PathBuf};
+use serde::{Deserialize, Serialize};
+use std::{
+    env, fs, io,
+    path::{Path, PathBuf},
+};
 use thiserror::Error;
+
+use crate::files;
 
 pub mod config_handler;
 
+pub static DIR_NAME: &'static str = "episko";
+pub static DB_FILE_NAME: &'static str = "cache.db";
+pub static CONFIG_FILE_NAME: &'static str = "config.toml";
+
 pub type Result<T> = std::result::Result<T, Error>;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
+    pub database_path: PathBuf,
     pub directories_to_load: Vec<PathBuf>,
     pub files_to_load: Vec<PathBuf>,
-    pub database_path: PathBuf,
 }
 
 impl Config {
-    pub fn add_saved_file(&mut self, file: &Path) -> Result<()> {
-        Ok(())
+    pub fn try_default() -> Result<Self> {
+        let database_path = Self::generate_db_path()?;
+        let database_dir = database_path.parent().ok_or(Error::Directory)?;
+
+        if !database_dir.exists() {
+            fs::create_dir_all(database_dir)?;
+        }
+
+        Ok(Self {
+            database_path,
+            directories_to_load: vec![],
+            files_to_load: vec![],
+        })
     }
 
-    pub fn add_saved_directory(&mut self, dir: &Path) -> Result<()> {
-        Ok(())
+    pub fn add_saved_file(&mut self, file: &Path) {
+        self.files_to_load.push(file.to_path_buf())
+    }
+
+    pub fn add_saved_directory(&mut self, dir: &Path) {
+        self.directories_to_load.push(dir.to_path_buf())
+    }
+
+    fn generate_db_path() -> Result<PathBuf> {
+        #[cfg(unix)]
+        {
+            return env::var("XDG_CACHE_HOME")
+                .map(PathBuf::from)
+                .map(|p| p.join(DIR_NAME).join(DB_FILE_NAME))
+                .or_else(|_| {
+                    env::var("HOME")
+                        .map(PathBuf::from)
+                        .map(|p| p.join(".cache").join(DIR_NAME).join(DB_FILE_NAME))
+                })
+                .map_err(|_| Error::Directory);
+        }
+
+        #[cfg(windows)]
+        {
+            return env::var("LOCALAPPDATA")
+                .map(PathBuf::from)
+                .map(|p| p.join(CONFIG_DIR_NAME).join(DB_FILE_NAME))
+                .map_err(|_| Error::Directory);
+        }
+
+        #[cfg(not(any(unix, windows)))]
+        {
+            return Err(Error::UnknownOs(env::consts::OS.to_string()));
+        }
     }
 }
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("tbc")]
-    Generic,
-}
+    #[error("unknown os: `{0}")]
+    UnknownOs(String),
 
+    #[error("unable to find config dir")]
+    Directory,
+
+    #[error("an io error occured")]
+    Io(#[from] io::Error),
+
+    #[error("failed to interact with file")]
+    File(#[from] files::Error),
+}
--- episko_lib/src/config/config_handler.rs
+++ episko_lib/src/config/config_handler.rs
@@ -1,16 +1,81 @@
-use std::path::{Path, PathBuf};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
-use super::{Config, Result};
+use crate::{config::CONFIG_FILE_NAME, files::File};
 
-pub struct ConfigHandler;
+use super::{Config, Error, Result, DIR_NAME};
 
-impl ConfigHandler {
-    pub fn load_config() -> Result<Config> {
-        todo!()
-    }
-
-    pub fn save_config() -> Result<()> {
-        todo!()
-    }
+pub struct ConfigHandler {
+    config_path: PathBuf,
 }
 
+impl ConfigHandler {
+    pub fn new() -> Result<Self> {
+        let config_path = Self::get_config_dir()?;
+        if !config_path.exists() {
+            fs::create_dir_all(&config_path)?;
+        }
+
+        Ok(Self { config_path })
+    }
+
+    pub fn load_config(&self) -> Result<Config> {
+        let config_file = self.config_path.join(CONFIG_FILE_NAME);
+
+        if !config_file.exists() {
+            let config = Config::try_default()?;
+            config.write_file(&config_file)?;
+            return Ok(config);
+        }
+
+        Ok(Config::from_file(&config_file)?)
+    }
+
+    pub fn save_config(&self, config: &Config) -> Result<()> {
+        let config_file = self.config_path.join(CONFIG_FILE_NAME);
+
+        Ok(config.write_file(&config_file)?)
+    }
+
+    /// Retrieve the default directory for the config location.
+    ///
+    /// On Unix-like systems the directory is located at:
+    /// - `$XDG_CONFIG_DIR/episko` if `$XDG_CONFIG_DIR` is set
+    /// - `$HOME/.config/episko` otherwise
+    ///
+    /// On Windows systems the config directory is located at:
+    /// - `%APPDATA%\episko`
+    ///
+    /// # Errors
+    /// - [`Error::Directory`] if none of the relevant environmental variables are set
+    /// - [`Error::UnknownOs`] if the os is not windows or unix-like
+    fn get_config_dir() -> Result<PathBuf> {
+        #[cfg(unix)]
+        {
+            return env::var("XDG_CONFIG_DIR")
+                .map(PathBuf::from)
+                .map(|p| p.join(DIR_NAME))
+                .or_else(|_| {
+                    env::var("HOME")
+                        .map(PathBuf::from)
+                        .map(|p| p.join(".config").join(DIR_NAME))
+                })
+                .map_err(|_| Error::Directory);
+        }
+
+        #[cfg(windows)]
+        {
+            return env::var("APPDATA")
+                .map(PathBuf::from)
+                .map(|p| p.join(CONFIG_DIR_NAME))
+                .map_err(|_| Error::Directory);
+        }
+
+        #[cfg(not(any(unix, windows)))]
+        {
+            return Err(Error::UnknownOs(env::consts::OS.to_string()));
+        }
+    }
+}
