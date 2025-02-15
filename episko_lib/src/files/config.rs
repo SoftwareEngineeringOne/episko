12a3fc831ddaa91704e8cf8071ef32f858366c5e
Simon <simon21.blum@gmail.com>
2025-02-15 10:59:01 +0100

    feat: add "File" trait implementation for config

---
+++ episko_lib/src/files/config.rs
@@ -0,0 +1,13 @@
+use crate::config::Config;
+
+use super::{file_handler::FileHandler, File, Result};
+
+impl File for Config {
+    fn write_file(&self, path: &std::path::Path) -> Result<()> {
+        FileHandler::write_file(&self, path)
+    }
+
+    fn from_file(path: &std::path::Path) -> Result<Self> {
+        FileHandler::read_file(path)
+    }
+}
