9689b14a0557ee82d407b47a5e94254e48c044d3
Simon <simon21.blum@gmail.com>
2025-02-15 09:23:33 +0100

    refactor: update structs for config

--- episko_lib/src/config.rs
+++ episko_lib/src/config.rs
@@ -1,9 +1,29 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use thiserror::Error;
 
-pub mod config_controller;
+pub mod config_handler;
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub struct Config {
     pub directories_to_load: Vec<PathBuf>,
     pub files_to_load: Vec<PathBuf>,
-    pub database_directory: PathBuf,
+    pub database_path: PathBuf,
 }
+
+impl Config {
+    pub fn add_saved_file(&mut self, file: &Path) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn add_saved_directory(&mut self, dir: &Path) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("tbc")]
+    Generic,
+}
+
