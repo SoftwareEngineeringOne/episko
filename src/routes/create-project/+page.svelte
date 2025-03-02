c411ad1319f3461b8d33089576306807160297b1
benoeckl <ben@oeckl.com>
2025-03-02 18:09:13 +0100

    feat: implement create project page

--- src/routes/create-project/+page.svelte
+++ src/routes/create-project/+page.svelte
@@ -1,1 +1,22 @@
-<h1 class="text-2xl font-bold mb-4">Create Project</h1>
+<script lang="ts">
+	import ProjectForm from '$lib/components/project/ProjectForm.svelte';
+	import type { ProjectData } from '$lib/types';
+
+	let projectData: ProjectData = {
+		directory: '',
+		title: '',
+		categories: '',
+		languages: '',
+		preferredIDE: '',
+		buildSystem: '',
+		repositoryLink: '',
+		filePath: ''
+	};
+
+	function handleCreate(submittedData: ProjectData): void {
+		console.log('Creating project with data:', submittedData);
+		// TODO: Creation logic
+	}
+</script>
+
+<ProjectForm formTitle="Create New Project" projectData={projectData} onSubmit={handleCreate} />
