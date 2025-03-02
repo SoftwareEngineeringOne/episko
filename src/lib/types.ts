5b08debb1005ce5c6aed8043d2e9d81d41c98501
benoeckl <ben@oeckl.com>
2025-03-02 18:04:32 +0100

    feat: add project data interface

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export interface ProjectData {
+	directory: string;
+	title: string;
+	categories: string;
+	languages: string;
+	preferredIDE: string;
+	buildSystem: string;
+	repositoryLink: string;
+	filePath: string;
+}
